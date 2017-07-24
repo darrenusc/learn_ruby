4c2ffb3a4e9290d9e92bb91413990b08eadf83bf
Darren Chan <darrenusc@gmail.com>
2017-07-23 18:03:06 -0700

    Test complete

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,40 @@
 #write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(array)
+  total = 0
+  array.each do |n|
+    total += n
+  end
+  return total
+end
+
+def multiply(*num)
+  product = 1
+  num.each do |n|
+    product *= n
+  end
+  return product
+end
+
+def power(base,exp)
+  base**exp
+end
+
+def factorial(num)
+  product = 1
+  if num == 0
+    return product
+  else
+    (1..num).each do |n|
+      product *= n
+    end
+  end
+  return product
+end
