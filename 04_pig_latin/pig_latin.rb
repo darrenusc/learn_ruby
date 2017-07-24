e382900f2777f1515d0124476b218ec125f0650e
Darren Chan <darrenusc@gmail.com>
2017-07-24 00:09:21 -0700

    Test complete

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,24 +1,24 @@
 #write your code here
 def translate(string)
   words = string.split(" ")
-  #puts words
+
   concat = []
   words.map! do |s|
-    first_vowel = s.index(/[aeiou]/)
-    puts first_vowel
 
-    if first_vowel.nil? or first_vowel == 0
+    first_vowel = s.index(/[aeiou]/)
+    if first_vowel.nil? || first_vowel == 0
       s += "ay"
     else
+      if s[first_vowel] == "u" && s[first_vowel-1] == "q"
+        first_vowel += 1
+      end
       first_vowel -= 1
       s += "#{s[0..(first_vowel)]}ay"
       s.slice!(0..(first_vowel))
     end 
+    
     concat.push(s)
   end
 
-  #puts concat
   return concat.join(" ")
 end
-
-#puts translate("banana")
