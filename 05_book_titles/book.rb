c0f10b3a0e69b3f8ff890cdb2a9841702390f541
Darren Chan <darrenusc@gmail.com>
2017-08-01 16:23:18 -0700

    05_book_titles test complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
-class Book
 # write your code here
+class Book
+  attr_accessor :title
+
+  def title=(new_title)
+    little_words = [#articles
+                  "a", "an", "the", 
+                  #simple prepositions
+                  "about", "above", "across", "after", "against", "along", "among", "around", "as", "at",
+                  "before", "behind", "below", "beneath", "beside", "between", "beyond", "by", "concerning", "despite", 
+                  "down", "during", "except", "excepting", "for", "from", "in", "inside", "into", "like", 
+                  "near", "next", "of", "off", "on", "onto", "out", "outside", "over", "past", 
+                  "regarding", "round", "since", "through", "throughout", "till", "to", "toward", "under", "underneath", 
+                  "unlike", "until", "up", "upon", "with", "within", "without", 
+                  #conjunctions
+                  "and", "but", "or", "nor", "so", "yet"]
+    
+    # Capitalize all words except litte_words
+    words = new_title.split.each do |s| 
+        s.capitalize! unless little_words.include?(s)
+    end
+    # Capitalize first word if it was in little_words
+    words[0].capitalize!
+    
+    @title = words.join(" ")
+  end
+
 end
