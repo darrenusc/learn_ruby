4ae5d0ad784282f787f97e474c9c4c16695b31ff
Darren Chan <darrenusc@gmail.com>
2017-08-03 20:58:59 -0700

    06_timer test complete

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
   #write your code here
+  attr_writer :seconds
+  
+  def seconds
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+    minutes = @seconds % 3600 / 60
+    seconds = @seconds % 60
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end 
+
+  
+  def padded(time)
+    if time < 10
+      "0#{time}"
+    else
+      "#{time}"
+    end
+  end
+
 end
